e56f62aff1195d7d910ad35d61ac67dc220fe6cd
hanafathiyah <13520047@std.stei.itb.ac.id>
2024-05-19 15:46:07 +0700

    add vercel

--- api/server.py
+++ api/server.py
@@ -7,10 +7,6 @@ from praktikum.chatbot import findAnswer
 app = Flask(__name__)
 CORS(app, origins="http://localhost:5173")
 
-@app.route('/members',  methods=['GET'])
-def members():
-  return jsonify({"members": ["Member1", "Member2", "Member3"]})
-
 @app.route('/chatbot_regex', methods=['POST'])
 def handle_chatbot_regex_post():
   question =  json.loads(request.data)
